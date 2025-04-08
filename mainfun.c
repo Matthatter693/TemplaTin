6fc90e645ddba748ccd315e8f139a83ed0f6c490
Matthatter693 <pra060903@gmail.com>
2025-04-08 23:51:12 +0530

    main body function

---
+++ mainfun.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"header.h"
+
+void main(int argc,char*argv[]){
+
+    //checking for the argument semantics
+    if(argc!=2){
+        printf("Wrong usage:\n");
+        return;
+    }
+
+    //adding help and version commands
+
+    if(argv[1][0]=='-'){
+        if(argv[1][1]=='v'){
+            printf("TemplaTin (Template Creator Shell Utility by matthatter693)\n");
+            printf("Version 1.0\n");
+            printf("This is a free software, redistributable under GPL V2.0\nFor queries <pra060903.@gmail.com>\n");
+            return;
+        }
+    }
+
+    FILE *fp,*fp1;
+
+    //file existence check
+    if(fopen(argv[1],"r")){
+        printf("file %s is already present\n",argv[1]);
+        return;
+    }
+
+    //creating a file in case of its absence
+    fp1=fopen(argv[1],"w");
+    fp=fopen("templates/cpptemp","r");
+    word_copy(fp,fp1);
+    printf("The file %s is created\n",argv[1]);
+    fclose(fp);
+    fclose(fp1);
+
+}
