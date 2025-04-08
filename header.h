c49547fd95101dee18aa165f95687d7a04836265
Matthatter693 <pra060903@gmail.com>
2025-04-08 23:52:26 +0530

    header file

---
+++ header.h
@@ -0,0 +1,5 @@
+extern void print(char **FILE_BUF,int Rows);
+extern void FILE_BUF_DEALLOC(char **FILE_BUF,int Rows);
+extern char ** FILE_BUF_ALLOC(int Rows,int Cols);
+extern void word_copy(FILE *fp,FILE *fp1);
+
