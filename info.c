386bd2c5669568304725605d4ccca6a15b80af7e
Matthatter693 <pra060903@gmail.com>
2025-04-10 14:45:55 +0530

    info printing file

---
+++ info.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+//prints info of the TemplaTin
+void pr_info(void){
+printf("TemplaTin (Template Creator Shell Utility by matthatter693)\n");
+printf("Version 1.0\n");
+printf("This is a free software, redistributable under GPL V2.0\nFor queries <pra060903.@gmail.com>\n");
+}
+
