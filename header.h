797210b60ae1c5842d8a69981a6fe5558300a71c
Matthatter693 <pra060903@gmail.com>
2025-04-11 22:38:26 +0530

    Added pr_help to header

--- header.h
+++ header.h
@@ -3,3 +3,4 @@ extern void FILE_BUF_DEALLOC(char **FILE_BUF,int Rows);
 extern char ** FILE_BUF_ALLOC(int Rows,int Cols);
 extern void word_copy(FILE *fp,FILE *fp1);
 extern void pr_info(void);
+extern void pr_help(void);
