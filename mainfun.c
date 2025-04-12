c5f7d07f2c294db9545725c57939205e2b5f5775
Matthatter693 <pra060903@gmail.com>
2025-04-12 23:51:01 +0530

    added file search based on extension

--- mainfun.c
+++ mainfun.c
@@ -10,7 +10,7 @@ void main(int argc,char*argv[])
 
     if(argc<2)
     {
-        printf("The minimum argument count should be 3\n Run tin -h for more info");
+        printf("The minimum argument count should be 3\n Run tin -h for more info\n");
         return;
     }
 
@@ -39,14 +39,18 @@ void main(int argc,char*argv[])
 
     FILE *TEMP_FILE_P=NULL; //For reading temp file
     FILE *DEST_FILE_P=NULL; //For writing into destination file
+    char path[50]="../templates/";
+    char pathtemp[50];
+    char *p;
 
 //An iterative condition for iterating through the arguments creating targets..
 
     for(int i=1;i<argc;i++)
     {
+    strcpy(pathtemp,path);
+    printf("pathtemp=%s\n",pathtemp);
 
     //Checking if the file is already present if not continues to check next argument
-    
         if(fopen(argv[i],"r"))
         {
             printf("file %s is already present\n",argv[i]);
@@ -54,34 +58,20 @@ void main(int argc,char*argv[])
         }
 
     //creating a file in case of its absence
-        //CPP support..
-        if(strstr(argv[i],".cpp"))
+        //File is created with respect to extension;
+        if((p=strstr(argv[i],"."))!=NULL)
         {
-            printf("reading cpptemp..\n");
-            TEMP_FILE_P=fopen("../templates/cpptemp","r");
+            printf("reading tmp..\n");
+            TEMP_FILE_P=fopen(strcat(strcat(pathtemp,p),"tmp"),"r");
             if(TEMP_FILE_P==NULL){
                 printf("Template not present..\n");
             }
         }
-
-        //C support...
-
-        else if(strstr(argv[i],".c"))
-        {
-            printf("reading ctemp..\n");
-            TEMP_FILE_P=fopen("../templates/ctemp","r");
-            if(TEMP_FILE_P==NULL){
-                printf("Template not present");
-            }
-        }
-
         //incase of no extensions..
-
         else{
             printf("bad arguments..\n");
             return;
         }
-
         //safety check
         if(TEMP_FILE_P==NULL){
             printf("fp is buggy\n");
@@ -89,13 +79,11 @@ void main(int argc,char*argv[])
         }
         
         //Opening target with write condition and writing template contents..
-
         DEST_FILE_P=fopen(argv[i],"w");
         word_copy(TEMP_FILE_P,DEST_FILE_P);
         printf("The file %s is created\n",argv[i]);
 
         //closing File pointers
-
         fclose(TEMP_FILE_P);
         fclose(DEST_FILE_P);
     }
