ccb15faa6c1234f0cea8bb4114301d35f287f138
Matthatter693 <pra060903@gmail.com>
2025-04-16 13:59:52 +0530

    Added memory usage optimizations for multi files

--- mainfun.c
+++ mainfun.c
@@ -6,7 +6,7 @@
 void main(int argc,char*argv[])
 {
 
-//checking conditions for the correct argument input
+    //checking conditions for the correct argument input
 
     if(argc<2)
     {
@@ -14,7 +14,7 @@ void main(int argc,char*argv[])
         return;
     }
 
-//adding help and version display support
+    //adding help and version display support
 
     if(argv[1][0]=='-')
     {
@@ -35,56 +35,86 @@ void main(int argc,char*argv[])
         }
     }
 
-// File pointers for FILE IO 
+    // File pointers for FILE IO 
 
     FILE *TEMP_FILE_P=NULL; //For reading temp file
     FILE *DEST_FILE_P=NULL; //For writing into destination file
-    char path[50]="../templates/";
-    char pathtemp[50];
-    char *p;
+    
+    //Path arrays
+    char path[100]="../templates/";
+    char pathtemp[100];
 
-//An iterative condition for iterating through the arguments creating targets..
+    //Other miscellaneous variables for storing info
+    int str,flag=0;
+    char ext[20];
+    char *p;
+    struct info temp;
+    char **buf;
+
+    //An iterative condition for iterating through the arguments creating targets..
 
     for(int i=1;i<argc;i++)
     {
-    strcpy(pathtemp,path);
-    printf("pathtemp=%s\n",pathtemp);
+        strcpy(pathtemp,path);
 
-    //Checking if the file is already present if not continues to check next argument
+        //Checking if the file is already present if not continues to check next argument
         if(fopen(argv[i],"r"))
         {
             printf("file %s is already present\n",argv[i]);
             continue;
         }
 
-    //creating a file in case of its absence
+        //creating a file in case of its absence
         //File is created with respect to extension;
+
         if((p=strstr(argv[i],"."))!=NULL)
         {
-            printf("reading tmp..\n");
-            TEMP_FILE_P=fopen(strcat(strcat(pathtemp,p),"tmp"),"r");
-            if(TEMP_FILE_P==NULL){
-                printf("Template not present..\n");
+
+            /*This condition check is for multi file purposes
+             * It will check the args one by one
+             * when change occurs only then buf is created and allocated*/
+
+            if((str=strcmp(ext,p))!=0)
+            {
+                strcpy(ext,p);
+
+                TEMP_FILE_P=fopen(strcat(strcat(pathtemp,p),"tmp"),"r");
+
+                if(TEMP_FILE_P==NULL){
+                    printf("Template not present..\n");
+                    return;
+                }
+
+                //The flag is to ensure that memory is freed in the first creation of the buffer
+
+                if(flag==1){
+                    FILE_BUF_DEALLOC(buf,temp);
+                }
+
+                //Copying file content to buffer and closing the file
+                
+                temp=file_info(TEMP_FILE_P);
+                buf=FILE_BUF_ALLOC(temp);
+                word_copy(TEMP_FILE_P,buf,temp);
+                flag=1;
+                fclose(TEMP_FILE_P);
             }
         }
-        //incase of no extensions..
-        else{
-            printf("bad arguments..\n");
-            return;
-        }
-        //safety check
-        if(TEMP_FILE_P==NULL){
-            printf("fp is buggy\n");
-            return;
-        }
-        
-        //Opening target with write condition and writing template contents..
-        DEST_FILE_P=fopen(argv[i],"w");
-        word_copy(TEMP_FILE_P,DEST_FILE_P);
-        printf("The file %s is created\n",argv[i]);
 
+        //incase of no extensions..
+        else
+        {
+            printf("Add valid extensions to the files.\n");
+            return;
+        }
+
+        //Opening target with write condition and writing template contents..
+
+        DEST_FILE_P=fopen(argv[i],"w");
+        word_write(DEST_FILE_P,buf,temp);
+        printf("The file %s is created\n",argv[i]);
+        
         //closing File pointers
-        fclose(TEMP_FILE_P);
         fclose(DEST_FILE_P);
     }
 
