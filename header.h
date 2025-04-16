360684ae9122c010a52518c2eff2b05edcd7d51e
Matthatter693 <pra060903@gmail.com>
2025-04-16 14:01:16 +0530

    Added a struct and conditional compilation flags

--- header.h
+++ header.h
@@ -1,6 +1,20 @@
-extern void print(char **FILE_BUF,int Rows);
-extern void FILE_BUF_DEALLOC(char **FILE_BUF,int Rows);
-extern char ** FILE_BUF_ALLOC(int Rows,int Cols);
-extern void word_copy(FILE *fp,FILE *fp1);
+
+#ifndef header
+#define header
+
+struct info{
+    int Newline_Count;
+    int Final_Count;
+};
+
+extern void print(char **FILE_BUF,struct info);
+extern void FILE_BUF_DEALLOC(char **FILE_BUF,struct info);
+extern char ** FILE_BUF_ALLOC(struct info);
+extern void word_copy(FILE *,char ** ,struct info);
 extern void pr_info(void);
 extern void pr_help(void);
+extern void word_write(FILE *,char **,struct info);
+extern struct info file_info(FILE *fp);
+extern void word_copy1(FILE *fp,char ** FILE_BUF,int Final_Count);
+
+#endif
